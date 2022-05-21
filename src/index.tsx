8fbdabc07e26695e8b3236e033ed9b8366711671
Jared <wilbrahamjared@gmail.com>
2022-05-21 19:50:47 -0400

    README Setup

--- src/index.tsx
+++ src/index.tsx
@@ -10,9 +10,9 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
